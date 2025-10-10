5863e529097229771d766222ac847298534bbd56
maedi <maediprichard@gmail.com>
2025-10-11 00:50:28 +1100

    Put type expression in different file

--- lib/low_type.rb
+++ lib/low_type.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'type_expression'
+
 module LowType
   class InvalidTypeError < StandardError; end;
   class RequiredValueError < StandardError; end;
@@ -129,58 +131,8 @@ module LowType
     end
   end
 
-  class TypeExpression
-    attr_reader :type, :default_value
-
-    def initialize(type:)
-      @types = [type]
-      @default_value = :LOW_TYPE_UNDEFINED
-    end
-
-    def |(expression)
-      if expression.class == ::LowType::TypeExpression
-        @types = @types + expression.types
-        @default_value = expression.default_value
-      elsif ::LowType.value?(expression)
-        @default_value = expression
-      else
-        @types << expression
-      end
-
-      self
-    end
-
-    def required?
-      @default_value == :LOW_TYPE_UNDEFINED
-    end
-
-    def validate!(arg:, name:)
-      if arg.nil? && required?
-        raise ::LowType::RequiredValueError, "Missing value of required type [#{@types.join(',')}] for '#{name}'"
-      end
-
-      raise ::LowType::InvalidTypeError, "Invalid type '#{arg.class}' for '#{name}'" unless @types.include?(arg.class)
-    end
-  end
-
   class ValueExpression; end
 
   class Boolean; end
   class KeyValue; end
-end
-
-class Object
-  class << self
-    # "|" is not defined on Object class and this is the most compute-efficient way to achieve our goal (world peace).
-    # "|" bitwise operator on Integer is not defined when the receiver is an Integer class, so we are not in conflict.
-    def |(expression)
-      if expression.class == ::LowType::TypeExpression
-        expression | self
-        expression
-      else
-        type_expression = ::LowType::TypeExpression.new(type: self)
-        type_expression | expression        
-      end
-    end
-  end
 end
---
+++ lib/type_expression.rb
@@ -0,0 +1,51 @@
+module LowType
+  class TypeExpression
+    attr_reader :type, :default_value
+
+    def initialize(type:)
+      @types = [type]
+      @default_value = :LOW_TYPE_UNDEFINED
+    end
+
+    def |(expression)
+      if expression.class == ::LowType::TypeExpression
+        @types = @types + expression.types
+        @default_value = expression.default_value
+      elsif ::LowType.value?(expression)
+        @default_value = expression
+      else
+        @types << expression
+      end
+
+      self
+    end
+
+    def required?
+      @default_value == :LOW_TYPE_UNDEFINED
+    end
+
+    def validate!(arg:, name:)
+      if arg.nil? && required?
+        raise ::LowType::RequiredValueError, "Missing value of required type [#{@types.join(',')}] for '#{name}'"
+      end
+
+      raise ::LowType::InvalidTypeError, "Invalid type '#{arg.class}' for '#{name}'" unless @types.include?(arg.class)
+    end
+  end
+end
+
+class Object
+  class << self
+    # "|" is not defined on Object class and this is the most compute-efficient way to achieve our goal (world peace).
+    # "|" bitwise operator on Integer is not defined when the receiver is an Integer class, so we are not in conflict.
+    def |(expression)
+      if expression.class == ::LowType::TypeExpression
+        expression | self
+        expression
+      else
+        type_expression = ::LowType::TypeExpression.new(type: self)
+        type_expression | expression        
+      end
+    end
+  end
+end
