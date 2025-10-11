a46e1cefffacdf466673da8aefc713896e69e9ab
maedi <maediprichard@gmail.com>
2025-10-11 22:33:22 +1100

    Test an untyped class with LowType included

---
+++ spec/fixtures/hello.rb
@@ -0,0 +1,24 @@
+require_relative '../../lib/low_type.rb'
+
+class Hello
+  include LowType
+
+  def initialize(greeting, name)
+    @greeting = greeting
+    @name = name
+  end
+
+  def arg(greeting)
+    greeting
+  end
+
+  def arg_and_default_value(greeting = 'Hello')
+    greeting
+  end
+
+  class << self
+    def say_goodbye(goodbye)
+      goodbye
+    end
+  end
+end
---
+++ spec/hello_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require_relative 'fixtures/hello.rb'
+
+RSpec.describe Hello do
+  subject(:hello) { described_class.new(greeting, name) }
+
+  let(:greeting) { 'Hey' }
+  let(:name) { 'Mate' }
+
+  describe '#initialize' do
+    it 'instantiates a class' do
+      expect { hello }.not_to raise_error
+    end
+  end
+
+  describe '#arg' do
+    it 'passes through the argument' do
+      expect(hello.arg('Hi')).to eq('Hi')
+    end
+
+    context 'when no arg provided' do
+      it 'raises an argument error' do
+        expect { hello.arg }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#arg_and_default_value' do
+    it 'passes through the argument' do
+      expect(hello.arg_and_default_value('Howdy')).to eq('Howdy')
+    end
+
+    context 'when no arg provided' do
+      it 'provides the default value' do
+        expect(hello.arg_and_default_value).to eq('Hello')
+      end
+    end
+  end
+end
