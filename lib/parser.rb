f8a37a724a08fc73447a69430844cd09ca0f54ab
maedi <maediprichard@gmail.com>
2025-10-12 20:09:18 +1100

    Use an abstract syntax tree

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,26 +1,47 @@
+require 'prism'
 require_relative 'param_proxy'
 require_relative 'type_expression'
 
 module LowType
+  class MethodVisitor < Prism::Visitor
+    attr_reader :defined_methods
+
+    def initialize
+      @defined_methods = []
+    end
+
+    def visit_def_node(node)
+      @defined_methods << node
+
+      super # Continue walking the tree.
+    end
+  end
+
   class Parser
     class << self
-      def redefine_methods(file_path:, klass:)
-        Module.new do
-          # TODO: Use an AST.
-          File.readlines(file_path).each do |file_line|
-            method_line = file_line.strip
-            next unless method_line.start_with?('def ') && method_line.include?('(')
+      def parse(file_path:, klass:)
+        method_visitor = MethodVisitor.new
 
-            method_name, args = method_line.delete_prefix('def ').split(/[()]/)
-            method_name = method_name.to_sym
+        root_node = Prism.parse_file(file_path).value
+        root_node.accept(method_visitor)
+
+        method_visitor.defined_methods
+      end
+
+      def redefine_methods(file_path:, klass:)
+        defined_methods = Parser.parse(file_path:, klass:)
+
+        Module.new do
+          defined_methods.each do |method|
+            args = method.parameters.slice
 
             proxy_method = eval("-> (#{args}) {}")
             required_args, required_kwargs = Parser.required_args(proxy_method)
 
-            klass.low_params[method_name] = Parser.type_expressions_from_params(proxy_method, args, required_args, required_kwargs)
+            klass.low_params[method.name] = Parser.type_expressions_from_params(proxy_method, args, required_args, required_kwargs)
             
-            define_method(method_name) do |*args, **kwargs|
-              klass.low_params[method_name].each do |param_proxy|
+            define_method(method.name) do |*args, **kwargs|
+              klass.low_params[method.name].each do |param_proxy|
                 arg = param_proxy.position ? args[param_proxy.position] : kwargs[param_proxy.name]
                 arg = param_proxy.type_expression.default_value if arg.nil? && param_proxy.type_expression.default_value != :LOW_TYPE_UNDEFINED
                 param_proxy.type_expression.validate!(arg:, name: param_proxy.name)
