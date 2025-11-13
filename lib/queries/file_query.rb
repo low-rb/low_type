44449251f5b24b7b06f561ca57f9e6c5dca37d1b
maedi <maediprichard@gmail.com>
2025-11-14 10:57:25 +1100

    Rename line_with to line_from

--- lib/queries/file_query.rb
+++ lib/queries/file_query.rb
@@ -2,18 +2,18 @@ module LowType
   class FileQuery
     class << self
       def file_path(klass:)
-        includer_line = line_with_class(klass:) || line_with_include || ''
+        includer_line = line_from_class(klass:) || line_from_include || ''
         includer_line.split(':').first || ''
       end
 
       private
 
-      def line_with_class(klass:)
+      def line_from_class(klass:)
         class_name = klass.to_s.split(':').last # Also remove the module namespaces from the class.
         caller.find { |callee| callee.end_with?("<class:#{class_name}>'") }
       end
 
-      def line_with_include
+      def line_from_include
         caller.find { |callee| callee.end_with?("include'") }
       end
     end
