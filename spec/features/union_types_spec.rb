66fab0c9e23cf01b58c428b8228ca84f45647cc6
maedi <maediprichard@gmail.com>
2026-09-16 00:33:22 +1000

    Shimmed vs stripped error types

--- spec/features/union_types_spec.rb
+++ spec/features/union_types_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe UnionTypes do
   subject(:union_types) { described_class.new }
 
   describe '#multiple_typed_args' do
-    it 'passes through both arguments types' do
+    it 'passes through both arguments' do
       expect(union_types.multiple_typed_args('Shalom')).to eq('Shalom')
       expect(union_types.multiple_typed_args(123)).to eq(123)
     end
@@ -23,12 +23,9 @@ RSpec.describe UnionTypes do
     end
 
     context 'when no arg is provided' do
-      let(:error_message) do
-        "Invalid argument type 'NilClass' for parameter 'greeting'. Valid types: 'String | Integer'"
-      end
-
       it 'raises an argument error' do
-        expect { union_types.multiple_typed_args }.to raise_error(Low::ArgumentTypeError, error_message)
+        # When shimmed the error message will be from LowType, when stripped standard Ruby.
+        expect { union_types.multiple_typed_args }.to raise_error(ArgumentError)
       end
     end
   end
